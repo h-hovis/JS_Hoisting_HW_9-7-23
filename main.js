dbbed29d0e1d2746688ae89039125cd813adb29d
hslape <awinter71417@gmail.com>
2023-09-05 15:24:28 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+// video task 1
+ book('The Magicians Nephew');
+
+ function book(title) {
+    console.log(`The book is called ${title}`);
+ }
+
+// ANSWER: the book has not been declared yet, so it will not run
+
+// video task 2
+// let book = 'The Silver Chair';
+
+// console.log(book);
+
+// ANSWER: expected outcome: reference error. I would fix this by moving the console.log below the declaration
